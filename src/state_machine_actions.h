fa6de3170cbbd51971f1c795212e95204e8f3be8
maxamo203 <69518405+maxamo203@users.noreply.github.com>
2025-05-04 17:12:45 -0300

    set actions (#4)

--- src/state_machine_actions.h
+++ src/state_machine_actions.h
@@ -6,70 +6,71 @@ typedef void (*action)();
 
 void showHourTimerLCD(void *)
 {
- short displayingCurrentTime = 1;
+  short displayingCurrentTime = 1;
 
- while (1)
- {
-  if (uxSemaphoreGetCount(showTimerSemaphore) > 0)
+  while (1)
   {
-   getLocalTime(&timeinfo, 10); // Actualiza la hora
-   // Semáforo disponible (no lo toma)
-   char mensaje[32]; // Ajustá el tamaño según el contenido esperado
+    if (uxSemaphoreGetCount(showTimerSemaphore) > 0)
+    {
+      getLocalTime(&timeinfo, 10); // Actualiza la hora
+      // Semáforo disponible (no lo toma)
+      char mensaje[32]; // Ajustá el tamaño según el contenido esperado
 
-   if (displayingCurrentTime == 1)
-   {
-    snprintf(mensaje, sizeof(mensaje), "Hora: %02d:%02d", timeinfo.tm_hour, timeinfo.tm_min);
-   }
-   else
-   {
-    snprintf(mensaje, sizeof(mensaje), "Proxima toma: \n%02d:%02d", schedule[nextPeriod].tm_hour, schedule[nextPeriod].tm_min);
-   }
+      if (displayingCurrentTime == 1)
+      {
+        snprintf(mensaje, sizeof(mensaje), "Hora: %02d:%02d", timeinfo.tm_hour, timeinfo.tm_min);
+      }
+      else
+      {
+        snprintf(mensaje, sizeof(mensaje), "Proxima toma: \n%02d:%02d", schedule[nextPeriod].tm_hour, schedule[nextPeriod].tm_min);
+      }
 
-   writeLCD(mensaje);
-   displayingCurrentTime *= -1; // Cambia el estado de la variable
-   Serial.println("Show hour timer LCD");
-   vTaskDelay(1000 / portTICK_PERIOD_MS);
+      writeLCD(mensaje);
+      displayingCurrentTime *= -1; // Cambia el estado de la variable
+      Serial.println("Show hour timer LCD");
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    else
+    {
+      // Semáforo ocupado: tomar con bloqueo
+      xSemaphoreTake(showTimerSemaphore, portMAX_DELAY);
+    }
   }
-  else
-  {
-   // Semáforo ocupado: tomar con bloqueo
-   xSemaphoreTake(showTimerSemaphore, portMAX_DELAY);
-  }
- }
 }
 // Actions
 void initialize()
 {
- /*
- int scheduleSetup[MAX_DAYS][MAX_PILLS_PER_DAY] = {
-     {6, 14, 21}, // Domingo Mañana, Domingo Tarde, Domingo Noche
-     {6, 0, 0},
-     {6, 0, 0},
-     {0, 0, 0},
-     {0, 0, 0},
-     {0, 0, 0},
-     {0, 0, 0}};
-     */
- configSetup();
+  /*
+  int scheduleSetup[MAX_DAYS][MAX_PILLS_PER_DAY] = {
+      {6, 14, 21}, // Domingo Mañana, Domingo Tarde, Domingo Noche
+      {6, 0, 0},
+      {6, 0, 0},
+      {0, 0, 0},
+      {0, 0, 0},
+      {0, 0, 0},
+      {0, 0, 0}};
+      */
+  configSetup();
 
- // Lee archivo con horarios y dias
- // Calcula el proximo horario y dia
- // Pasa al estado awaiting reminder time
- fisicalSetup();
- attachInterrupt(LIMIT_SWITCH_PIN, detectMovingLimitSwitch, RISING);
- setupWifi();
- setupTime();
+  // Lee archivo con horarios y dias
+  // Calcula el proximo horario y dia
+  // Pasa al estado awaiting reminder time
+  fisicalSetup();
+  attachInterrupt(LIMIT_SWITCH_PIN, detectMovingLimitSwitch, RISING);
+  setupWifi();
+  setupTime();
 
- createNewScheduledTimer();
- timeEventsQueue = xQueueCreate(MAX_EVENTS_QUEUE, sizeof(events));
- showTimerSemaphore = xSemaphoreCreateMutex();
- xTaskCreate(showHourTimerLCD, "showHourTimerLCD", 2048, NULL, 1, NULL);
+  createNewScheduledTimer();
+  timeEventsQueue = xQueueCreate(MAX_EVENTS_QUEUE, sizeof(events));
+  showTimerSemaphore = xSemaphoreCreateMutex();
+  xTaskCreate(showHourTimerLCD, "showHourTimerLCD", 2048, NULL, 1, NULL);
 }
 
 void noScheduleSet();
 void settingSchedule();
 void awaitingTimer();
-void scanning() {
+void scanning()
+{
   stopMotor();
   DebugPrint("Scanning");
 
@@ -77,10 +78,13 @@ void scanning() {
 };
 void pillDetected();
 void noPillDetected();
+void doseTaken();
+void stopReturning();
 void doseSkipped();
+
 void returnToHome()
 {
- startMotorLeft();
+  startMotorLeft();
 };
 void error();
 void none();
@@ -91,16 +95,48 @@ void error()
 void none()
 {
 }
-
+void doseTaken()
+{
+}
+void stopReturning()
+{
+  Serial.println("Stop returning...");
+}
 void awaitingTimer()
 {
- Serial.println("Awaiting timer...");
- xSemaphoreGive(showTimerSemaphore);
+  Serial.println("Awaiting timer...");
+  xSemaphoreGive(showTimerSemaphore);
 }
 void moving()
 {
- Serial.println("Moving...");
- xSemaphoreTake(showTimerSemaphore, 0);
- setDayAndPeriod();
- startMotorRight();
+  Serial.println("Moving...");
+  xSemaphoreTake(showTimerSemaphore, 0);
+  setDayAndPeriod();
+  startMotorRight();
+}
+void scanning()
+{
+  Serial.println("Scanning...");
+}
+void pillDetected()
+{
+  Serial.println("Pill detected...");
+}
+void noPillDetected()
+{
+  Serial.println("No pill detected...");
+}
+void doseSkipped()
+{
+  Serial.println("Dose skipped...");
+}
+void settingSchedule()
+{
+  Serial.println("Setting schedule...");
+  xSemaphoreGive(showTimerSemaphore);
+}
+void noScheduleSet()
+{
+  Serial.println("No schedule set...");
+  xSemaphoreGive(showTimerSemaphore);
 }
